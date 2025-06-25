84b74fd32b53f9f8a62eb77d28a60066dfdd54ba
飞走的纸飞机 <1749748955@qq.com>
2025-06-25 01:10:39 +0000

    update

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@ yarn build
 git checkout doc
 rm -rf 404.html  assets  blog  docs  img  index.html  markdown-page  sitemap.xml
 mv build/* .
+git add .
+git commit  -m 'update'
+git push origin doc
+git checkout master
+git push origin master
