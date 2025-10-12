c3faa021701e9af62204049e5ee1d3593b0c77c5
飞走的纸飞机 <33304691+wangyang1749@users.noreply.github.com>
2025-10-12 13:25:41 +0000

    add ant

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -8,6 +8,7 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 import Giscus from '@giscus/react';
+import { Button, Card, Col, Row } from 'antd';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -32,20 +33,65 @@ function HomepageHeader() {
 // import brave from  "./static/img/brave.png"
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
+  const [isXs, setIsXs] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setIsXs(window.innerWidth < 576);
+    handleResize(); // 初始化
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main style={{ maxWidth: "1000px", margin: "2rem auto" }}>
-        <p>
-          BRAVE is a visual bioinformatics parameter generator that allows users to configure and run analysis workflows through an intuitive interface. It runs seamlessly in isolated Docker containers, ensuring reproducibility and easy deployment.
-        </p>
-        <div style={{ display: "flex", justifyContent: "center", margin: "2rem 0" }}>
-          <img src={require('@site/static/img/brave.png').default} alt="" />
-        </div>
+      <main style={{ maxWidth: "1800px", margin: "2rem auto" }}>
 
-        
+        <Row>
+          <Col xs={24} sm={6} md={6} lg={6} xl={6}>
+
+            <Card >
+              <p>
+                BRAVE is a visual bioinformatics parameter generator that allows users to configure and run analysis workflows through an intuitive interface. It runs seamlessly in isolated Docker containers, ensuring reproducibility and easy deployment.
+              </p>
+              <div style={{ display: "flex", justifyContent: "center", margin: "2rem 0" }}>
+                <img src={require('@site/static/img/brave.png').default} alt="" />
+              </div>
+
+            </Card>
+
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}
+            style={{
+              padding: isXs ? 0 : "0 1rem",
+              marginTop: isXs ? "1rem" : 0,
+            }}
+          >
+
+            <Card >
+              <Giscus
+                id="comments"
+                repo="pybrave/brave"
+                repoId="R_kgDOPAOzKw"
+                category="General"
+                categoryId="DIC_kwDOPAOzK84Cwi0X"
+                mapping="number"
+                term="2"
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="top"
+                theme="light_high_contrast"
+                lang="en"
+                loading="lazy"
+              />
+
+            </Card>
+          </Col>
+          <Col xs={24} sm={6} md={6} lg={6} xl={6}>
+            <Card ></Card>
+          </Col>
+        </Row>
         {/* <script src="https://giscus.app/client.js"
         data-repo="pybrave/brave"
         data-repo-id="R_kgDOPAOzKw"
